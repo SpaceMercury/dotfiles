e4db1c32812121b32b0ae16c6d2c1fb98aa0584e
SpaceMercury <polinboston@gmail.com>
2025-06-14 01:01:04 +0200

    working top bar

--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+
+kilalll waybar || true
+
 swww init &
 
 dunst &
 
 nm-applet --indicator &
 #blueman-applet &
-waybar &
+nohup waybar --config "$HOME/.config/waybar/config.jsonc" --style "$HOME/.config/waybar/style.css" >/dev/null 2>&1 &
+
 
